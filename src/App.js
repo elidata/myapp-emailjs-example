a28467ed1703d3dc26ae309760c130db69e4ce2c
Eliedaat Adler <elidaatad@post.jce.ac.il>
2023-04-13 18:32:57 +0300

    example of sending emailjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import './SendAMail.js' ;
+import EmailContactForm from './SendAMail.js';
+import React, { useRef } from "react";
+import emailjs, { init } from "@emailjs/browser";
 
 function App() {
+  init("user_xxxxxxxxxxxxxxxxxxx");
+  const form = useRef();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('NoCharamot', 'template_smin43a',
+    form.current, 'ji7gBPq28AGAyRUgW').then (   
+       (result) => {
+        alert("Message Sent Successfully");
+        console.log(result.text);
+      },
+      (error) => {
+        console.log(error.text);
+      }
+    );
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form onSubmit={handleSubmit} ref={form}>
+        <h1 className="text-center">Registration Form</h1>
+        <div className="form-row">
+          <div className="form-group col-md-6">
+            <label htmlFor="First Name">First Name</label>
+            <input type="text" className="form-control" name="firstname" />
+          </div>
+          <div className="form-group col-md-6">
+            <label htmlFor="Last Name">Last Name</label>
+            <input type="text" className="form-control" name="lastname" />
+          </div>
+          <div className="form-group col-12">
+            <label htmlFor="inputAddress">Address</label>
+            <input
+              type="text"
+              className="form-control"
+              id="inputAddress"
+              placeholder="1234 Main St"
+              name="user_address"
+            />
+          </div>
+          <div className="form-group col-md-6">
+            <label htmlFor="message">message</label>
+            <textarea
+              type="text"
+              className="form-control"
+              id="inputmessage4"
+              name="user_message"
+            />
+          </div>
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Sign in
+        </button>
+      </form>
     </div>
   );
 }
-
 export default App;
